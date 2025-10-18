b456d3f149d09ffabe693d00d872d84ac630df53
russkyc <jrcc1023@gmail.com>
2025-10-18 12:17:52 +0800

    :sparkles: Update theme styles, add new controls, and enhance input components

---
+++ Russkyc.UI.Avalonia/Converters/FocusBorderRadiusConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace Russkyc.UI.Avalonia.Converters;
+
+public class FocusBorderRadiusConverter : IValueConverter
+{
+    public static FocusBorderRadiusConverter Instance => new();
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not CornerRadius thickness) return null;
+        var padding = 2;
+        return new CornerRadius(thickness.TopLeft+padding, thickness.TopRight+padding, thickness.BottomRight+padding, thickness.BottomLeft+padding);
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
