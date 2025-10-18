5dc31780bbb65fc223b3deb84a2747e802636ba5
russkyc <jrcc1023@gmail.com>
2025-10-18 13:03:00 +0800

    :sparkles: Enhance ListBox component with new styles and disabled state, update ListBoxView layout

--- Russkyc.UI.Avalonia.Gallery/Views/Pages/ListBoxView.axaml.cs
+++ Russkyc.UI.Avalonia.Gallery/Views/Pages/ListBoxView.axaml.cs
@@ -1,6 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
 
 namespace Russkyc.UI.Avalonia.Gallery.Views.Pages;
 
@@ -9,5 +7,33 @@ public partial class ListBoxView : UserControl
     public ListBoxView()
     {
         InitializeComponent();
+        CodeBlock.Text = """
+                         <Border Classes="Card">
+                             <StackPanel
+                                 Width="300"
+                                 Margin="20"
+                                 HorizontalAlignment="Center"
+                                 VerticalAlignment="Center"
+                                 Spacing="8">
+                                 <ListBox HorizontalAlignment="Stretch">
+                                     <ListBoxItem>Apple</ListBoxItem>
+                                     <ListBoxItem>Banana</ListBoxItem>
+                                     <ListBoxItem>Blueberry</ListBoxItem>
+                                     <ListBoxItem>Grapes</ListBoxItem>
+                                     <ListBoxItem>Pineapple</ListBoxItem>
+                                 </ListBox>
+                                 <ListBox
+                                     HorizontalAlignment="Stretch"
+                                     IsEnabled="False">
+                                     <ListBoxItem>Apple</ListBoxItem>
+                                     <ListBoxItem>Banana</ListBoxItem>
+                                     <ListBoxItem>Blueberry</ListBoxItem>
+                                     <ListBoxItem>Grapes</ListBoxItem>
+                                     <ListBoxItem>Pineapple</ListBoxItem>
+                                 </ListBox>
+                             </StackPanel>
+                         </Border>
+                         """;
     }
 }
+
