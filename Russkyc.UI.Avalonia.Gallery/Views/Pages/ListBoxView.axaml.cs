5429bd505cd4b381062799087cae1be2f61cfc8e
russkyc <jrcc1023@gmail.com>
2025-10-18 12:42:04 +0800

    :sparkles: Add listbox styles

---
+++ Russkyc.UI.Avalonia.Gallery/Views/Pages/ListBoxView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Russkyc.UI.Avalonia.Gallery.Views.Pages;
+
+public partial class ListBoxView : UserControl
+{
+    public ListBoxView()
+    {
+        InitializeComponent();
+    }
+}
